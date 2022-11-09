6426b50f5c6965a18acc6d8001832d453a4a5259
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2022-11-09 15:16:00 +0100

    Top categories dona

---
+++ src/components/top/TopCate.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import TopCart from './TopCart'
+import './Style.css'
+
+const TopCate = () => {
+  return (
+    <>
+        <section className="topCat background">
+          <div className="container">
+            <div className="heading d_flex">
+              <div className="heading-left row f_flex">
+                <i className="fa fa-border-all"></i>
+                <h2>Categorie più acquistate</h2>
+              </div>
+              <div className="heading-right row">
+                <span>Mostra tutto</span>
+                <i className="fa fa-caret-right"></i>
+
+              </div>
+            </div>
+            <TopCart />
+          </div>
+        </section>
+    </>
+  )
+}
+
+export default TopCate
