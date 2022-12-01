c227fe653bfcb09123e24b02bb35a9e8ec644e4a
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2022-12-01 20:38:02 +0100

    Removed Backend folder

--- backend/mysql-dump/db_init.sql
+++
@@ -1,8 +0,0 @@
-USE mrtecno;
-
-CREATE TABLE utente (
-    username VARCHAR(50) NOT NULL, 
-    password VARCHAR(50) NOT NULL,
-    PRIMARY KEY(username));
-
-INSERT INTO utente VALUE("admin", "admin");
