302b023b0835d49eb8c974411a2fc488684335ba
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2023-01-24 01:19:52 +0100

    Deleted summit section on admin order page

--- frontend/src/components/adminDashboard/Pages/AdminPanel/AdminPanel.jsx
+++ frontend/src/components/adminDashboard/Pages/AdminPanel/AdminPanel.jsx
@@ -54,47 +54,6 @@ const AdminPanel = () => {
       <div className="adminPanel-heading">
         <h2 className='adminPanel-title'>Dashboard</h2>
       </div>
-      <div className="row">
-        <div className="col-lg-4">
-          <div className="card card-body mb-4 shadow-sm">
-            <article className="icontext">
-              <span className="icon icon-sm rounded-circle sales">
-                <i className="fas fa-euro-sign"></i>
-              </span>
-              <div className="text">
-                <h6 className='Total-sales'>Totale vendite</h6>
-                <span>400€</span>
-              </div>
-            </article>
-          </div>
-        </div>
-        <div className="col-lg-4">
-          <div className="card card-body mb-4 shadow-sm">
-            <article className="icontext">
-              <span className="icon icon-sm rounded-circle orders">
-                <i className="fas fa-euro-sign"></i>
-              </span>
-              <div className="text">
-                <h6 className='Total-sales'># di ordini</h6>
-                <span>400</span>
-              </div>
-            </article>
-          </div>
-        </div>
-        <div className="col-lg-4">
-          <div className="card card-body mb-4 shadow-sm">
-            <article className="icontext">
-              <span className="icon icon-sm rounded-circle products">
-                <i className="fas fa-euro-sign"></i>
-              </span>
-              <div className="text">
-                <h6 className='Total-sales'># di prodotti</h6>
-                <span>400</span>
-              </div>
-            </article>
-          </div>
-        </div>
-      </div>
       <div className="card hide mb-4 shadow-sm">
         <div className="card-body">
           <h5 className="card-title">Ultimi ordini</h5>
