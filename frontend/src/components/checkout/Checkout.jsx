df9c2a7b88fc1f195f4b0f850e6592ab549cab7a
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2023-02-22 20:07:19 +0100

    Paypal Buttons spinners on checkout

--- frontend/src/components/checkout/Checkout.jsx
+++ frontend/src/components/checkout/Checkout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Style.css';
-import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
+import { PayPalScriptProvider, PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
 import axios from '../../api/axios'
 import useAuth from "../../hooks/useAuth";
 import { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Cookies from 'universal-cookie';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 
 const Checkout = ({ cleanCart, cartItem }) => {
@@ -94,6 +95,100 @@ const Checkout = ({ cleanCart, cartItem }) => {
             };
         })
     }
+    
+    const ButtonWrapper = () => {
+
+        const [{ isPending }] = usePayPalScriptReducer();
+
+        return (
+            <>            
+                {
+                    
+                    
+                    isPending ? 
+                        
+                
+                            <div style={{ display: 'flex', justifyContent: 'center', margin: '30px' }}>
+                                <ClipLoader
+                                color={'#0f3460'}
+                                loading={isPending}
+                                size={50}
+                                />
+                            </div>  
+                                
+                        : null
+                        
+                }
+            
+            
+            <PayPalButtons
+                style={{ 
+                    layout: "vertical",
+                    shape: "pill",
+                }}
+                createOrder={(data, actions) => {
+                    return actions.order.create({
+                        purchase_units: [
+                            {
+                                description: 'Ordine MrTecno',
+                                amount: {
+                                    value: totalPrice,
+                                    breakdown: {
+                                        item_total: {
+                                            currency_code: "EUR",
+                                            value: netTotal,
+                                        },
+                                        shipping: {
+                                            value: shipping_cost,
+                                            currency_code: "EUR",
+                                        },
+                                        tax_total: {
+                                            value: payPalCommissions,
+                                            currency_code: "EUR",
+                                        }
+                                    },
+                                }, "items": getItemArray(),
+                            },
+                        ],
+                    });
+                }}
+
+                onApprove={(data, actions) => {
+                    return actions.order.capture().then(function (details) {
+                        newOrder(details);
+                        cookies.remove('cartItem');
+                        cleanCart();
+                        navigate('/orderSuccess', { replace: true });
+                    })
+                }}
+
+                onClick={async () => {
+
+                    const response = await axios.post(PRODUCT_AVAILABILITY_URL, 
+                        { 
+                            cart: JSON.parse(JSON.stringify(cookies.get('cartItem')) || '[]')
+                        
+                        },
+                        {
+                            headers: {
+                                'Authorization': `Bearer ${auth?.accessToken}`
+                            },
+                            withCredentials: true
+                        }
+                    );
+
+                    if (!response.data) { 
+                        const from = location.state?.from?.pathname || "/cart";
+                        navigate(from, { replace: true }); 
+                        alert("Un prodotto non è più disponibile")
+                    }
+                    
+                }}
+
+            />            
+            </>
+        )
+    }
 
     return (
         <section className='checkout'>
@@ -125,78 +220,22 @@ const Checkout = ({ cleanCart, cartItem }) => {
                         <div className="paypal-button-container">
                             <div className="checkout-txt">
                                 <h1 className='checkout-span'>Scegli il metodo di pagamento</h1>
-                            </div>                
-            <PayPalScriptProvider options={
-                    {
-                        "client-id": "ATZNHaB7fylPEKToWL-_Cnzb2WIdfxNHMK7JFACI0K48o6vV2UukiwQ72XFU-fG7dKK3I9bi6RT1_qzy",
-                        currency: "EUR"
-                    }}>
-                <PayPalButtons
-                    style={{ 
-                        layout: "vertical",
-                        shape: "pill",
-                     }}
-                    createOrder={(data, actions) => {
-                        return actions.order.create({
-                            purchase_units: [
-                                {
-                                    description: 'Ordine MrTecno',
-                                    amount: {
-                                        value: totalPrice,
-                                        breakdown: {
-                                            item_total: {
-                                                currency_code: "EUR",
-                                                value: netTotal,
-                                            },
-                                            shipping: {
-                                                value: shipping_cost,
-                                                currency_code: "EUR",
-                                            },
-                                            tax_total: {
-                                                value: payPalCommissions,
-                                                currency_code: "EUR",
-                                            }
-                                        },
-                                    }, "items": getItemArray(),
-                                },
-                            ],
-                        });
-                    }}
-
-                    onApprove={(data, actions) => {
-                        return actions.order.capture().then(function (details) {
-                            newOrder(details);
-                            cookies.remove('cartItem');
-                            cleanCart();
-                            navigate('/orderSuccess', { replace: true });
-                        })
-                    }}
-
-                    onClick={async () => {
-
-                        const response = await axios.post(PRODUCT_AVAILABILITY_URL, 
-                            { 
-                                cart: JSON.parse(JSON.stringify(cookies.get('cartItem')) || '[]')
+                            </div> 
                             
-                            },
-                            {
-                                headers: {
-                                    'Authorization': `Bearer ${auth?.accessToken}`
-                                },
-                                withCredentials: true
-                            }
-                        );
-
-                        if (!response.data) { 
-                            const from = location.state?.from?.pathname || "/cart";
-                            navigate(from, { replace: true }); 
-                            alert("Un prodotto non è più disponibile")
-                        }
-                        
-                    }}
-
-                />
-            </PayPalScriptProvider>
+                            
+                               
+                                    
+                                    <PayPalScriptProvider options={
+                                        {
+                                            "client-id": "ATZNHaB7fylPEKToWL-_Cnzb2WIdfxNHMK7JFACI0K48o6vV2UukiwQ72XFU-fG7dKK3I9bi6RT1_qzy",
+                                            currency: "EUR"
+                                        }}>
+                                    <ButtonWrapper/>
+                                </PayPalScriptProvider>
+                                    
+                            
+                                        
+            
         </div>
                     
             )
