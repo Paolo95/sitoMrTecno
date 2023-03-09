9dc4f0bee623b7d8837f3f1582fe91e68b6eedaa
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2023-03-09 19:12:00 +0100

    Latest barters section done

--- frontend/src/components/adminDashboard/Pages/AdminPanel/AdminPanel.jsx
+++ frontend/src/components/adminDashboard/Pages/AdminPanel/AdminPanel.jsx
@@ -10,14 +10,17 @@ const AdminPanel = () => {
 
   const { auth } = useAuth();
   const [orderList, setOrderList] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [barterList, setBarterList] = useState([]);
+  const [orderLoading, setOrderLoading] = useState(false);
+  const [barterLoading, setBarterLoading] = useState(false);
   const GET_ORDER_LIST = 'api/order/getRecentOrders';
+  const GET_BARTER_LIST = 'api/barter/getRecentBarters';
 
   useEffect(() => {
 
     const orderList = async () => {
 
-      setLoading(true);
+      setOrderLoading(true);
 
       try {
 
@@ -33,7 +36,7 @@ const AdminPanel = () => {
           }
         );
 
-        setLoading(false);
+        setOrderLoading(false);
         setOrderList(response.data);
 
       } catch (err) {
@@ -48,9 +51,43 @@ const AdminPanel = () => {
 
     }
 
-    orderList();
+    const barterList = async () => {
 
-  }, [orderList.length, auth.accessToken])
+      setBarterLoading(true);
+
+      try {
+
+        const response = await axios.post(GET_BARTER_LIST,
+          {
+
+          },
+          {
+            headers: {
+              'Authorization': `Bearer ${auth?.accessToken}`
+            },
+            withCredentials: true
+          }
+        );
+
+        setBarterLoading(false);
+        setBarterList(response.data);
+
+      } catch (err) {
+        if (!err?.response) {
+          console.error('Server non attivo!');
+        } else if (err.response?.status === 500) {
+          console.error(err.response?.data);
+        } else {
+          console.error('Recupero permute fallito!');
+        }
+      }
+
+    }
+
+    orderList();
+    barterList();
+
+  }, [orderList.length, barterList.lenght, auth.accessToken])
 
 
   return (
@@ -58,15 +95,15 @@ const AdminPanel = () => {
       <div className="adminPanel-heading">
         <h2 className='adminPanel-title'>Dashboard</h2>
       </div>
-      <div className="card hide mb-4 shadow-sm">
+      <div className="card mb-4 shadow-sm">
         <div className="card-body">
         <h5 className="card-title">Ultimi ordini</h5>
           {
-            loading ?
+            orderLoading ?
               <div style={{ display: 'flex', justifyContent: 'center', margin: '30px' }}>
                   <ClipLoader
                   color={'#0f3460'}
-                  loading={loading}
+                  loading={orderLoading}
                   size={50}
                   />
               </div>
@@ -83,13 +120,13 @@ const AdminPanel = () => {
                               {<Moment format='DD/MM/YYYY'>{value['order.order_date']}</Moment>}
                             </td>
                             <td>
-                              {value['order.user.email']}
+                              {value['order.user.username']}
                             </td>
                             <td>
-                              {value['order_total']} €
+                              <span>{value['order_total']}€</span>
                             </td>
                             <td>
-                              {value['order.order_status']}
+                              <span>{value['order.order_status']}</span>
                             </td>
                             <td className='dash-btn'>
                               <NavLink to={`/AdminDashboard/orders/editOrder/${value['order.id']}`}>
@@ -108,6 +145,56 @@ const AdminPanel = () => {
           }
           
         </div>
+
+        
+      </div>
+
+      <div className="card mb-4 shadow-sm">
+        <div className="card-body">
+        <h5 className="card-title">Ultime permute da valutare</h5>
+          {
+            barterLoading ?
+              <div style={{ display: 'flex', justifyContent: 'center', margin: '30px' }}>
+                  <ClipLoader
+                  color={'#0f3460'}
+                  loading={barterLoading}
+                  size={50}
+                  />
+              </div>
+              :
+              <>
+              <div className="table-div">
+                <table className="table">
+                  <tbody>
+                    {
+                      barterList.map((value, index) => {
+                        return (
+                          <tr key={index}>
+                            <td>
+                              {<Moment format='DD/MM/YYYY'>{value['barter_date']}</Moment>}
+                            </td>
+                            <td>
+                              {value['user.username']}
+                            </td>
+                            <td className='dash-btn'>
+                              <NavLink to={`/AdminDashboard/barters/editBarter/${value['id']}`}>
+                                <button>Dettaglio</button>
+                              </NavLink>
+                            </td>
+                          </tr>
+                        )
+                      })
+                    }
+                  </tbody>
+                </table>
+              </div>
+              
+              </>
+          }
+          
+        </div>
+
+        
       </div>
     </section>
 
